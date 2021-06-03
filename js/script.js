08943887a3890c68353c346d2644202d71681d6a
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-03 23:57:16 +0200

    js-lista-cognomi

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var surnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
 var surnameUser = surnames.push(prompt("Qual'è il tuo cognome?"));
 
-surnames.sort();
-document.getElementById("surnameOutput").innerHTML = surnames.join("<br>");
-var humanCount = surnames.indexOf(surnames.sort);
+for (var i = 0; i < surnames.length; i++) {
+    surnames.sort();
+    document.getElementById("surnameOutput").innerHTML = surnames.join("<br>");
+    var humanCount = surnames.indexOf(surnames.sort);
+}
